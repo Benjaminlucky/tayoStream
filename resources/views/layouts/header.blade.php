30a2599ab9d4e8a83887686b4fb39102424d48a9
Benjaminlucky <42000766+Benjaminlucky@users.noreply.github.com>
2022-11-22 20:09:41 +0100

    first commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,23 @@
+<div class="header-Section">
+        <div class="row mx-auto header-Section-Container">
+            <div class="col-md-6 section-left">
+                <div class="headerLinkLeft">
+                    <a href="#">Our Work</a>
+                    <a href="#">Blog</a>
+                </div>
+            </div>
+            <div class="col-md-6 section-right">
+                <div class="headerLinkRight">
+                    <a href="#">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>08053642425</p>
+                    </a>
+                    <a href="#">
+                        <i class="fa-solid fa-envelope"></i>
+                        <button>Get Free Demo </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
